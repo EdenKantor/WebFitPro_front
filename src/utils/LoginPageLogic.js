9a83191bf53a429951eff81f25715727fc11ae60
EdenKantor <ngeden7676@gmail.com>
2025-01-05 23:00:50 +0200

    Added new funcunality

--- src/utils/LoginPageLogic.js
+++ src/utils/LoginPageLogic.js
@@ -22,7 +22,7 @@ export const useLoginLogic = () => {
       return;
     }
 
-    const url = `https://web-fit-pro-front.vercel.app/login?username=${username}&password=${password}`;
+    const url = `https://web-fit-pro-back.vercel.app/login?username=${username}&password=${password}`;
 
     try {
       const response = await fetch(url, { method: 'GET' });
