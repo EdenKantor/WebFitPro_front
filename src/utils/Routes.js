7c6e8a0613e6c38d76476b21122bcce069bb30c8
IdoSakal <idosakal97@gmail.com>
2025-02-14 10:27:45 +0200

    Create Routes.js

---
+++ src/utils/Routes.js
@@ -0,0 +1,38 @@
+import { Route, Switch } from 'wouter'; 
+import FirstPage from '../layouts/FirstPage';
+import LoginPage from '../layouts/LoginPage';
+import Registration1Page from "../layouts/Registration1Page";
+import Registration2Page from "../layouts/Registration2Page";
+import UserHomePage from '../layouts/UserHomePage';
+import Discover1Page from '../layouts/Discover1Page';
+import Discover2Page from '../layouts/Discover2Page';
+import InfoPage from '../layouts/InfoPage';
+import ContinueRoutine from '../layouts/ContinueRoutine';
+import PendingUsers from '../layouts/PendingUsers';
+import ManageUsers from '../layouts/ManageUsers';
+import NotSoFast from '../layouts/NotSoFast';
+
+const routes = [
+  { path: "/", component: FirstPage },
+  { path: "/login", component: LoginPage },
+  { path: "/register1", component: Registration1Page },
+  { path: "/register2", component: Registration2Page },
+  { path: "/user-home", component: UserHomePage },
+  { path: "/discover1", component: Discover1Page },
+  { path: "/discover2", component: Discover2Page },
+  { path: "/my-info", component: InfoPage },
+  { path: "/routine", component: ContinueRoutine },
+  { path: "/PendingUsers", component: PendingUsers },
+  { path: "/ManageUsers", component: ManageUsers },
+  { path: "/NotSoFast", component: NotSoFast }
+];
+
+const RouterConfig = () => (
+  <Switch>
+    {routes.map(({ path, component }) => (
+      <Route key={path} path={path} component={component} />
+    ))}
+  </Switch>
+);
+
+export default RouterConfig;
