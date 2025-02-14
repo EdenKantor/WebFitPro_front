2c00508e443f0d4c382c76109c8e0b77e1712621
IdoSakal <idosakal97@gmail.com>
2025-02-14 11:54:54 +0200

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,20 +1,27 @@
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { useEffect } from 'preact/hooks'; 
 import { initializeTheme } from './utils/ThemeLogic'; 
+import { Route, Switch } from 'wouter'; 
 import TopBar from './components/TopBar';
-import RouterConfig from './utils/Routes';
+import routes from './utils/Routes';
 
 function App() {
+
   useEffect(() => {
     initializeTheme(); 
   }, []); 
+  
 
   return (
     <div>
       <TopBar />
       <TransitionGroup>
         <CSSTransition key={location.key} timeout={500} classNames="fade">
-          {RouterConfig}
+          <Switch>
+              {routes.map(({ path, component }) => (
+                <Route key={path} path={path} component={component} />
+              ))}
+            </Switch>
         </CSSTransition>
       </TransitionGroup>
     </div>
